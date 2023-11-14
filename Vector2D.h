4ece7ce9f78d5cc61819325d829a750f3b132865
TrOllOchamO <barnabe.perrault@gmail.com>
2023-11-14 12:27:38 +0100

    Ajout d'un gitignore + implementation Vector2D

---
+++ Vector2D.h
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#ifndef VECTOR2D_H_
+#define VECTOR2D_H_
+
+class Vector2D {
+public:
+  float x;
+  float y;
+
+  Vector2D(float inputX, float inputY);
+
+  Vector2D &operator+=(const Vector2D &v);
+  Vector2D &operator-=(const Vector2D &v);
+  Vector2D &operator*=(const float &f);
+  Vector2D &normalize();
+
+  float lenght(const Vector2D &vect);
+  float dot(const Vector2D &vect1, const Vector2D &vect2);
+  Vector2D triple_cross_product(const Vector2D &vect1, const Vector2D &vect2, const Vector2D &vect3);
+  Vector2D normal_clockwise(const Vector2D &vect);
+  Vector2D normal_counter_clockwise(const Vector2D &vect);
+
+  friend Vector2D operator-(const Vector2D &v);
+  friend Vector2D operator-(const Vector2D &v1, const Vector2D &v2);
+  friend Vector2D operator+(const Vector2D &v1, const Vector2D &v2);
+  friend Vector2D operator*(const Vector2D &v, float f);
+  friend Vector2D operator*(float f, const Vector2D &v);
+  friend bool operator==(const Vector2D &v1, const Vector2D &v2);
+
+  friend std::ostream &operator<<(std::ostream &os, const Vector2D &v);
+};
+
+#endif /* !VECTOR2D_H_ */
