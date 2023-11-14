e15968b989ab1ad366b634eb97ffb9f0b9c4fae8
TrOllOchamO <barnabe.perrault@gmail.com>
2023-11-14 17:26:48 +0100

    suppression du todo + ajout du .gitkeep dans resources

--- README.md
+++ README.md
@@ -1,31 +1,3 @@
 # Flipper
-
-## TODO
-### Building blocks
-- [ ] Setup SFML on all our computers and check it works
-- [ ] Search for documentation generator and set it up for all our computers
-- [ ] Create a cmakefile
-- [ ] Search how to play souds and background music and test it
-- [ ] Search how to capture user inputs and test it
-- [ ] Search how to render things and test it
-- [ ] Search how to render sprites and test it
-- [ ] Serach how to render text and test it
-- [ ] Create a Vector2D class and implement all nessessary functions (sum, substraction, dot product...)
-- [ ] Create a simple gameloop
-- [ ] Implement game stages
-- [ ] Design a shape interface/class that could work nicely with gjk
-- [ ] Implement gjk
-- [ ] Implement epa
-- [ ] Implement physics and collision resolution
-
-### Minimum to call the game complete
-- [ ] A working flipper game
-- [ ] A pause menu
-- [ ] Starting menu
-- [ ] Music and sounds
-- [ ] Score system (with lives)
-
-### Extra
-- [ ] HightScore board ?
-- [ ] Implement the tilt mechanic ?
-- [ ] More maps ?
+## [Trello](https://trello.com/invite/b/uiBJdaBs/ATTI49c84a55fdc7950bdbf29fb98c939bae651FA67B/taleau-simple)
+## [Github](https://github.com/TrOllOchamO/Flipper)
--- Vector2D.cc
+++ Vector2D.cc
@@ -3,99 +3,90 @@
 
 Vector2D::Vector2D(float inputX, float inputY) : x(inputX), y(inputY) {}
 
-Vector2D& Vector2D::operator+=(const Vector2D& v) {
-    this->x += v.x;
-    this->y += v.y;
-    return *this;
+Vector2D &Vector2D::operator+=(const Vector2D &v) {
+  this->x += v.x;
+  this->y += v.y;
+  return *this;
 }
 
-Vector2D& Vector2D::operator-=(const Vector2D& v) {
-    this->x -= v.x;
-    this->y -= v.y;
-    return *this;
+Vector2D &Vector2D::operator-=(const Vector2D &v) {
+  this->x -= v.x;
+  this->y -= v.y;
+  return *this;
 }
 
-Vector2D& Vector2D::operator*=(const float& f) {
-    this->x *= f;
-    this->y *= f;
-    return *this;
+Vector2D &Vector2D::operator*=(const float &f) {
+  this->x *= f;
+  this->y *= f;
+  return *this;
 }
 
-Vector2D operator-(const Vector2D &v)
-{
-    Vector2D result = {-v.x, -v.y};
+Vector2D operator-(const Vector2D &v) {
+  Vector2D result = {-v.x, -v.y};
 
-    // avoid -0 shenanigans 
-    if (result.x == -0.0) { result.x = 0.0; }
-    if (result.y == -0.0) { result.y = 0.0; }
+  // avoid -0 shenanigans
+  if (result.x == -0.0) {
+    result.x = 0.0;
+  }
+  if (result.y == -0.0) {
+    result.y = 0.0;
+  }
 
-    return result;
+  return result;
 }
 
-Vector2D operator-(const Vector2D &v1, const Vector2D &v2)
-{
-    Vector2D result = v1;
-    return result -= v2;
+Vector2D operator-(const Vector2D &v1, const Vector2D &v2) {
+  Vector2D result = v1;
+  return result -= v2;
 }
 
-Vector2D operator+(const Vector2D &v1, const Vector2D &v2)
-{
-    Vector2D result = v1;
-    return result += v2;
+Vector2D operator+(const Vector2D &v1, const Vector2D &v2) {
+  Vector2D result = v1;
+  return result += v2;
 }
 
-Vector2D operator*(const Vector2D &v, float f)
-{
-    Vector2D result = v;
-    return result *= f;
+Vector2D operator*(const Vector2D &v, float f) {
+  Vector2D result = v;
+  return result *= f;
 }
 
-Vector2D operator*(float f, const Vector2D &v)
-{
-    Vector2D result = v;
-    return result *= f;
+Vector2D operator*(float f, const Vector2D &v) {
+  Vector2D result = v;
+  return result *= f;
 }
 
-std::ostream& operator<<(std::ostream& os, const Vector2D& v)
-{
-    os << "(" << v.x << ", " << v.y << ")";
-    return os;
+std::ostream &operator<<(std::ostream &os, const Vector2D &v) {
+  os << "(" << v.x << ", " << v.y << ")";
+  return os;
 }
 
-bool operator==(const Vector2D& v1, const Vector2D& v2)
-{
-    return (v1.x == v2.x && v1.y == v2.y);
+bool operator==(const Vector2D &v1, const Vector2D &v2) {
+  return (v1.x == v2.x && v1.y == v2.y);
 }
 
-Vector2D& Vector2D::normalize()
-{
-    const float vectorLength = std::sqrt(this->x*this->x + this->y*this->y);
-    this->x = this->x/vectorLength;
-    this->y = this->y/vectorLength;
-    return *this;
+Vector2D &Vector2D::normalize() {
+  const float vectorLength = std::sqrt(this->x * this->x + this->y * this->y);
+  this->x = this->x / vectorLength;
+  this->y = this->y / vectorLength;
+  return *this;
 }
 
-float Vector2D::dot(const Vector2D &vect1, const Vector2D &vect2)
-{
-    return vect1.x*vect2.x + vect1.y*vect2.y;
+float Vector2D::dot(const Vector2D &vect1, const Vector2D &vect2) {
+  return vect1.x * vect2.x + vect1.y * vect2.y;
 }
 
-Vector2D Vector2D::triple_cross_product(const Vector2D &vect1, const Vector2D &vect2, const Vector2D &vect3)
-{
-    return Vector2D::dot(vect1, vect3)*vect2 - Vector2D::dot(vect2, vect3)*vect1;
+Vector2D Vector2D::triple_cross_product(const Vector2D &vect1, const Vector2D &vect2, const Vector2D &vect3) {
+  return Vector2D::dot(vect1, vect3) * vect2 - Vector2D::dot(vect2, vect3) * vect1;
 }
 
-Vector2D Vector2D::normal_clockwise(const Vector2D &vect)
-{
-    return Vector2D(vect.y, -vect.x);
+Vector2D Vector2D::normal_clockwise(const Vector2D &vect) {
+  return Vector2D(vect.y, -vect.x);
 }
 
-Vector2D Vector2D::normal_counter_clockwise(const Vector2D &vect)
-{
-    return Vector2D(-vect.y, vect.x);
+Vector2D Vector2D::normal_counter_clockwise(const Vector2D &vect) {
+  return Vector2D(-vect.y, vect.x);
 }
 
-float Vector2D::lenght(const Vector2D &vect)
-{
-    return sqrt(vect.x*vect.x + vect.y*vect.y);
+float Vector2D::lenght(const Vector2D &vect) {
+  return sqrt(vect.x * vect.x + vect.y * vect.y);
 }
