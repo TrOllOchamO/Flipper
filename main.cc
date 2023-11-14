ab463ce55b5d39e4e64c76cf74a610384ce3047f
Herteit <antondolard@gmail.com>
2023-11-14 19:03:16 +0100

    Audio quand un utilisateur clic

--- main.cc
+++ main.cc
@@ -1,4 +1,6 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
 //juste un test pour voir si SFML+Cmake marche
 int main()
 {
@@ -6,13 +8,25 @@ int main()
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile("resources/gragas_2.wav")){
+         std::cerr << "Erreur lors du chargement du fichier audio" << std::endl;
+        return 1;
+    }
+    
+    sf::Sound sound;
+    sound.setBuffer(buffer);
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed){
                 window.close();
+            }else if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                // Jouer le son lorsque l'utilisateur clique
+                sound.play();
+            }
         }
 
         window.clear();
