e4c9be6698a4d73e5b55bacaefac19bd3b37a8fa
LeSaintRizBleu <mateo.galizzi05@gmail.com>
2023-11-29 15:49:23 +0100

    is_convex

--- Polygone.cc
+++ Polygone.cc
@@ -8,26 +8,22 @@ bool Polygone::add_point(Vector2D point) {
   return true;
 }
 
-// vvv ca fonctionne pas ca vvv
-double get_angle_from_points(Vector2D a, Vector2D b, Vector2D c){
-  double ra = (b.x-a.x)*(b.x-a.x) + (b.y-a.y)*(b.y-a.y);
-  double rb = (b.x-c.x)*(b.x-c.x) + (b.y-c.y)*(b.y-c.y);
-  double rc = (c.x-a.x)*(c.x-a.x) + (c.y-a.y)*(c.y-a.y);
-
-  double res = std::acos( (ra+rb-rc) / sqrt(4*ra*rb) ) * 180/3.141592653589793238462643383279502884;
-  std::cout << res << "\n";
-  return res;
-}
-
 bool Polygone::is_convex(){
   int n = points.size();
+  int comp = true;
   if(n<4){
     return true;
   }
+  if((points[1].x - points[0].x) * (points[2].y - points[1].y) - (points[1].y - points[0].y) * (points[2].x - points[1].x) < 0){
+    comp = false;
+  }
   for(int i=0; i<n; i++){
-    if(get_angle_from_points(points[i%n], points[i+1%n], points[i+2%n]) > 180.0 ){
-      return false;
-    }
+    Vector2D a = points[i%n];
+    Vector2D b = points[i+1%n];
+    Vector2D c = points[i+2%n];
+    double res = (b.x - a.x) * (c.y - b.y) - (b.y - a.y) * (c.x - b.x);
+    std::cout << res << "\n";
+    if((comp && res<0) || (!comp && res>=0)) return false;  
   }
   return true;
 }
--- main.cc
+++ main.cc
@@ -5,33 +5,25 @@
 #include "Polygone.h"
 
 int main() {
-    // sf::RenderWindow window(sf::VideoMode(400, 800), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(400, 800), "SFML works!");
 
-    // sf::Clock clock;
-    // Ball ball(Vector2D(100, 500), 8);
+    sf::Clock clock;
+    Ball ball(Vector2D(100, 500), 8);
  
-    // while (window.isOpen()) {
-    //     float dt = clock.restart().asSeconds();
+    while (window.isOpen()) {
+        float dt = clock.restart().asSeconds();
 
-    //     sf::Event event;
-    //     window.clear(sf::Color::Black);
-    //     while (window.pollEvent(event)) {
-    //         if (event.type == sf::Event::Closed){
-    //             window.close();
-    //         }
-    //     }
-    //     ball.move(dt);
-    //     ball.render(window);
-    //     window.display();
-    // }
-
-    Polygone p(Vector2D(100, 500));
-    p.add_point(Vector2D(1,1));
-    p.add_point(Vector2D(2,1));
-    p.add_point(Vector2D(3,1));
-    p.add_point(Vector2D(2,5));
-
-    std::cout << p.is_convex() << "\n";
+        sf::Event event;
+        window.clear(sf::Color::Black);
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+        ball.move(dt);
+        ball.render(window);
+        window.display();
+    }
 
     return 0;
 }
