de12d9a345779ca3df14b0e463cac31335eccce0
LeSaintRizBleu <mateo.galizzi05@gmail.com>
2023-11-29 16:22:16 +0100

    get center polygone

--- Polygone.cc
+++ Polygone.cc
@@ -29,7 +29,20 @@ bool Polygone::is_convex(){
 }
 
 Vector2D Polygone::get_center() const {
-  return Vector2D(0,0);
+  Vector2D sum_center(0,0);
+  double sum_weight = 0.0;
+  int n = points.size();
+  for(int i=0; i<n; i++){
+    Vector2D a = points[i-1 % n];
+    Vector2D b = points[i % n];
+    Vector2D c = points[i+1 % n];
+    double w = (b-c).length() + (b-a).length();
+    sum_center += b * w;
+    sum_weight += w;
+  }
+  sum_center.x/=sum_weight;
+  sum_center.y/=sum_weight;
+  return sum_center;
 }
 
 Vector2D Polygone::get_futhest_point(const Vector2D &direction) const {
