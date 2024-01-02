b3d6237396a731cfd1190f591d972d2f8ad81693
Riz <mateo.galizzi05@gmail.com>
2024-01-02 16:30:17 +0100

    aled

--- TestFlipper.cc
+++ TestFlipper.cc
@@ -1,10 +1,36 @@
 #include "gtest/gtest.h"
-#include "Collision.h"
+#include "Circle.h"
+#include "Vector2D.h"
 
 
-TEST(AutomatonExampleTest, Default) {
-  EXPECT_FALSE(false);
+
+// =========================================================
+//
+// CIRCLE
+//
+// =========================================================
+
+TEST(get_center, Both_Positive) {
+  Circle c(Vector2D(0, 0), 1);
+  auto center = c.get_center();
+  EXPECT_EQ(center, Vector2D(1, 1));
 }
+
+// TEST(get_center, Positive_Negative) {
+//   EXPECT_FALSE(false);
+// }
+
+// TEST(get_center, Negative_Positive) {
+//   EXPECT_FALSE(false);
+// }
+
+// TEST(get_center, Both_Negative) {
+//   EXPECT_FALSE(false);
+// }
+
+
+
+
 
 
 int main(int argc, char** argv) {
