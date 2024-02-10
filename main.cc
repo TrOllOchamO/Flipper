2ed27ae762c460c58e724a29ac53e98c9e22b552
Herteit <antondolard@gmail.com>
2024-02-10 17:03:56 +0100

    Première version menu

--- main.cc
+++ main.cc
@@ -12,25 +12,91 @@
 #define WINDOWS_WIDTH 400
 #define WINDOWS_HEIGHT 800
 
+enum class MenuState {
+    MainMenu,
+    GameRunning,
+    GameMenu,
+    MapSelection
+};
+
 int main() {
-  sf::RenderWindow window(sf::VideoMode(WINDOWS_WIDTH, WINDOWS_HEIGHT), "Flipper");
-  sf::Clock clock;
-  Inputs inputs;
+    sf::RenderWindow window(sf::VideoMode(WINDOWS_WIDTH, WINDOWS_HEIGHT), "Flipper");
+    sf::Clock clock;
+    Inputs inputs;
+    MenuState menuState = MenuState::MainMenu;
 
-  // on créer la map
-  Map map;
-  Loader::load_map_1(map);
-  
-  // load map
-  Game game(std::move(map));
-  
-  while (window.isOpen()) {
-    float dt = clock.restart().asSeconds();
-    inputs.update(window);
-    window.clear(sf::Color::Black);
+    // Création des boutons
+    sf::Font font;
+    //resources/arial.ttf
+    if (!font.loadFromFile("resources/arial.ttf")) {
+        std::cerr << "Failed to load font!" << std::endl;
+        return 1;
+    }
 
-    game.update(window, inputs, dt);
+    sf::Text launchButton("Lancer", font, 24);
+    sf::Text quitButton("Quitter", font, 24);
+    launchButton.setPosition(WINDOWS_WIDTH / 2 - 50, 300);
+    quitButton.setPosition(WINDOWS_WIDTH / 2 - 50, 400);
 
-    window.display();
-  }
+    sf::Text resumeButton("Reprendre", font, 24);
+    sf::Text mainMenuButton("Menu principal", font, 24);
+    resumeButton.setPosition(WINDOWS_WIDTH / 2 - 80, 300);
+    mainMenuButton.setPosition(WINDOWS_WIDTH / 2 - 100, 400);
+
+    // on crée la map
+    Map map;
+    Loader::load_map_1(map);
+
+    // load map
+    Game game(std::move(map));
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                if (menuState == MenuState::GameRunning) {
+                    menuState = MenuState::GameMenu;
+                } else if (menuState == MenuState::GameMenu) {
+                    menuState = MenuState::GameRunning;
+                }
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (menuState == MenuState::MainMenu) {
+                    if (launchButton.getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y))) {
+                        menuState = MenuState::GameRunning;
+                    } else if (quitButton.getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y))) {
+                        window.close();
+                    }
+                } else if (menuState == MenuState::GameMenu) {
+                    if (resumeButton.getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y))) {
+                        menuState = MenuState::GameRunning;
+                    } else if (mainMenuButton.getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y))) {
+                        menuState = MenuState::MainMenu;
+                    }
+                }
+            }
+        }
+
+        float dt = clock.restart().asSeconds();
+        inputs.update(window);
+        window.clear(sf::Color::Black);
+
+        if (menuState == MenuState::MainMenu) {
+            window.draw(launchButton);
+            window.draw(quitButton);
+        } else if (menuState == MenuState::GameRunning) {
+            game.update(window, inputs, dt);
+        } else if (menuState == MenuState::GameMenu) {
+            window.draw(resumeButton);
+            window.draw(mainMenuButton);
+        }
+
+        window.display();
+    }
+
+    return 0;
 }
